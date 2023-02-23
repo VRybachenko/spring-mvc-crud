c2b9855dae0331664ba712e0316c0dec6e79ae7d
vrybachenko <vrybache@softserveinc.com>
2023-02-23 18:53:27 +0200

    Added my project

---
+++ src/main/java/spring/mvc/course/config/controllers/FirstController.java
@@ -0,0 +1,57 @@
+package spring.mvc.course.config.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+
+//        System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodByePage() {
+        return "first/goodbye";
+    }
+
+    @GetMapping("/calcutator")
+    public String calcutator(@RequestParam(value = "a") int a, @RequestParam(value = "b") int b,
+                             @RequestParam(value = "action") String action, Model model) {
+
+        double result;
+
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "division":
+                result = a / (double) b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "addtion":
+                result = a + b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        model.addAttribute("result", result);
+
+        return "/first/calcutator";
+    }
+}
